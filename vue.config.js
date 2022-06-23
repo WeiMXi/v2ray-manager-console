5e1b2402432843b9911101fcd93dd8bcaeab05fe
iYu <bai.a.y@qq.com>
2022-06-23 19:27:59 +0800

    update make the project aviliable

--- vue.config.js
+++ vue.config.js
@@ -24,7 +24,8 @@ module.exports = {
    * In most cases please use '/' !!!
    * Detail: https://cli.vuejs.org/config/#publicpath
    */
-  publicPath: '/',
+  publicPath: './',
+  //publicPath: '/qiaomimi/v2_manager/',
   outputDir: 'dist',
   assetsDir: 'static',
   lintOnSave: process.env.NODE_ENV === 'development',
